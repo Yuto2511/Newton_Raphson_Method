84a5e36e88f1019b295261d1e2a9eddc347c2eaa
Yuto2511 <efmgh2511@outlook.jp>
2022-10-26 10:28:53 +0900

    first commit

---
+++ newton_2/newton_2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+
+#define EPS 0.0000001
+#define NMAX 100
+
+double f( double x );
+double df( double x );
+
+void newton( double x ){
+	int n = 0;
+	double d;
+
+	do {
+		d = -f(x) / df(x);
+		x += d;
+		n++;
+	} while( fabs(d) > EPS && n <= NMAX );
+	if( n == NMAX ) printf("error!\n");
+	else printf("x = %f\n", x);
+}
+
+int main()
+{
+	double x;
+
+	printf("input initial value -> ");
+	scanf("%lf", &x);
+	newton(x);
+
+	return 0;
+}
---
+++ newton_2/newton_func.c
@@ -0,0 +1,9 @@
+#include<math.h>
+
+double f( double x ){
+	return( x + cos(x) );
+}
+
+double df( double x ){
+	return( 1. - sin(x) );
+}
